a605f598ff3262811a3471275ea606e65d7a571d
sylviah <sylviah.rutto@gmail.com>
2019-07-19 17:25:19 +0300

    password

--- run.py
+++ run.py
@@ -8,5 +8,12 @@ def create_user(first_name,last_name, account,password):
     '''
     Function to create a new user
     '''
-    new_user = User(first_name,last_name, account,password)
+    new_user = User(fname,lname, account,password)
     return new_user
+
+def create_password(account, password):
+    '''
+    Function to create new password
+    '''
+    new_pass = Credentials(account, password)
+    return new_pass
