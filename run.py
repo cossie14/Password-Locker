65ee4aa293c95c8663515c2b537f2e0f379a38d0
sylviah <sylviah.rutto@gmail.com>
2019-07-19 17:35:24 +0300

    generating password

--- run.py
+++ run.py
@@ -54,9 +54,16 @@ def display_users():
     return User.display_users()
 
 
-def save_password(credentials):
+def generate_password():
     '''
-    Function that saves new password
+    Function that generates a password for the user
     '''
-    return credentials.save_password()
+    return Credentials.generatePassword()
+
+
+# def save_password(credentials):
+#     '''
+#     Function that saves new password
+#     '''
+#     return credentials.save_password()
 
