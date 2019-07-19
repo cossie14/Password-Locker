a40416b7668571b85e1a87f0bece0183d4116764
sylviah <sylviah.rutto@gmail.com>
2019-07-19 17:23:17 +0300

    creating a new user

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ from password import User
 from password import Credentials
 import random
 from getpass import getpass
+
+def create_user(first_name,last_name, account,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(first_name,last_name, account,password)
+    return new_user
