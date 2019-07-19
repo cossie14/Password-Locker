1f064a452f09c50936bb3140d439a3ea89133a3b
sylviah <sylviah.rutto@gmail.com>
2019-07-19 17:07:14 +0300

    generated password

--- password.py
+++ password.py
@@ -72,6 +72,12 @@ def display_users(cls):
     '''
     Class  generates and saves credentials for users
     '''
-    def __init__(self, account, password):
+       def __init__(firstname, account, password):
         self.password = password
+        self.account = account
+
+        def Password():
+        chars = '111111111111111111111111111122222222222226666666666666'
+        new_pass = ''.join(random.sample(chars, 5))
+        return new_pass
        
